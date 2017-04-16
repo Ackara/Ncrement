20d76aa4a9a8b7ae5ab9e1e69244587414cc855d
Ackara <ackara.dev@outlook.com>
2017-04-16 13:11:33 -0400

    Add Write-breakline cmdlet to utils.psm1

--- src/Buildbox.Utils/Buildbox.Utils.psm1
+++ src/Buildbox.Utils/Buildbox.Utils.psm1
@@ -1,6 +1,17 @@
-<#
-	My Function
-#>
-function Get-Function {
 
+function Write-Breakline () {
+	Param(
+		[string]$Title = ""
+	)
+
+	$line = "`n----------------------------------------------------------------------";
+	$limit = $line.Length;
+	if (-not [String]::IsNullOrEmpty($Title))
+	{
+		$line = $line.Insert(4, " $Title ");
+		if ($line.Length -gt $limit) { $line = $line.Substring(0, $limit); }
+	}
+
+	Write-Host $line;
+	Write-Host "";
 }
