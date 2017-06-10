20f199caaad6bd71a19351ea9ea522c662cd96b9
Ackara <ackara.dev@outlook.com>
2017-06-10 06:10:04 -0400

    Remove GetBranchSuffixCmdlet inheritance with cmdletbase

--- src/Buildbox.SemVer/Cmdlets/GetBranchSuffixCmdlet.cs
+++ src/Buildbox.SemVer/Cmdlets/GetBranchSuffixCmdlet.cs
@@ -3,26 +3,47 @@
 namespace Acklann.Buildbox.SemVer.Cmdlets
 {
     [Cmdlet(VerbsCommon.Get, "BranchSuffix")]
-    public sealed class GetBranchSuffixCmdlet : CmdletBase
+    public sealed class GetBranchSuffixCmdlet : Cmdlet
     {
+        [Parameter(Position = 2)]
+        [Alias(new string[] { "config", "settings", "c" })]
+        public string ConfigFile { get; set; }
+
         [Alias("branch", "bn", "key")]
-        [Parameter(ValueFromPipeline = true, Position = 0)]
+        [Parameter(ValueFromPipeline = true, Position = 1)]
         public string BranchName { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            if (string.IsNullOrWhiteSpace(ConfigFile)) ConfigFile = Settings.FindSettingsFile();
+            _config = Settings.Load(ConfigFile);
+        }
 
         protected override void ProcessRecord()
         {
-            if (Config.BranchToSuffixMap.ContainsKey(BranchName))
+            if (_config.BranchToSuffixMap.ContainsKey(BranchName))
             {
-                WriteObject(Config.BranchToSuffixMap[BranchName]);
+                WriteObject(_config.BranchToSuffixMap[BranchName]);
             }
-            else if (Config.BranchToSuffixMap.ContainsKey("*"))
+            else if (_config.BranchToSuffixMap.ContainsKey("*"))
             {
-                WriteObject(Config.BranchToSuffixMap["*"]);
+                WriteObject(_config.BranchToSuffixMap["*"]);
             }
             else
             {
                 WriteObject(null);
             }
         }
+
+        protected override void EndProcessing()
+        {
+            _config.Save(partial: true);
+        }
+
+        #region Private Members
+
+        private Settings _config;
+
+        #endregion Private Members
     }
 }
