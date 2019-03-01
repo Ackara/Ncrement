ca3a28be9db2041a2ff8ff0e6ad487d022d3ef4b
Ackara <ackara.dev@outlook.com>
2019-03-01 10:37:41 -0400

    Rename update-projectfile to update-ncrementprojectfile

--- src/Ncrement/Public/Update-NcrementProjectFile.ps1
+++ src/Ncrement/Public/Update-NcrementProjectFile.ps1
@@ -1,16 +1,43 @@
-﻿function Update-ProjectFile
+﻿<#
+.SYNOPSIS
+Updates a project file version number.
+
+.DESCRIPTION
+This cmdlet will modify a project file with the information in the specified [Manifest] object. If the project file passed is not known it will be ignored.
+
+.PARAMETER Manifest
+The file-path or instance of a [Manifest] object.
+
+.PARAMETER InputObject
+The project file. If the file type is unknown it will be ignored.
+
+.PARAMETER CommitMessage
+The git commit message.
+
+.PARAMETER Commit
+When present, any files modified by this cmdlet will be committed to source control.
+
+.EXAMPLE
+Get-ChildItem -Filter "*.csproj" | Update-NcrementProjectFile $manifest -Commit;
+This example will update the project file version number then commit the changes to source control.
+#>
+
+function Update-NcrementProjectFile
 {
 	[CmdletBinding(ConfirmImpact = "Medium", SupportsShouldProcess)]
 	Param(
 		[Parameter(Mandatory)]
+		[ValidateNotNull()]
 		$Manifest,
 
 		[Parameter(Mandatory, ValueFromPipeline)]
+		[ValidateNotNull()]
 		$InputObject,
 
 		[Alias('m', "Message")]
 		[string]$CommitMessage,
 
+		[Alias('c')]
 		[switch]$Commit
 	)
 
